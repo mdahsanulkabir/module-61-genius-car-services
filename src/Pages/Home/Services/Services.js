96d5773c0a5bcaa219d17dc848287c328cbe43be
ahsan <makabir98@yahoo.com>
2022-05-02 11:11:47 +0600

    fake data added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import repair1 from '../../../images/repair 1.png';
+
+const Services = () => {
+
+    const services = [
+        {id: 1, name: 'oil change', price : 100, description: '', img:repair1}
+    ];
+    return (
+        <div>
+            <h2>Services</h2>
+        </div>
+    );
+};
+
+export default Services;
