94c0e8b64bfd276711e0779adf3d61cac60abe92
ahsan <makabir98@yahoo.com>
2022-05-03 11:18:37 +0600

    environment variable included

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 import './Register.css'
 
 const Register = () => {
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+    }
     return (
         <div className='register-form'>
             <h2 style={{textAlign: 'center'}}>Please Register</h2>
-            <form action="">
+            <form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder='Your name'/>
                 <br />
                 <input type="email" name="email" id="" placeholder='Email Address' required/>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCr-3Hf9YpCW7Pp24DgQnWr-rsJ8imtwYM",
-  authDomain: "module60-genius-car-services.firebaseapp.com",
-  projectId: "module60-genius-car-services",
-  storageBucket: "module60-genius-car-services.appspot.com",
-  messagingSenderId: "916937693539",
-  appId: "1:916937693539:web:40869e02389a1454a7db56"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
